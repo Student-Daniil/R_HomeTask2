71fc27cc3ce223092bacb6077d3464413f0662d8
Student-Daniil <kerechanin.d@gmail.com>
2016-02-24 15:46:36 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,8 @@
+
+#we have two functions. First creates an object, that have matrix properties. 
+#Second helps us to minimize a memory usage checking out whether the inverse matrix was calculated before or no
+
+#makeCacheMatrix helps us to convert a simple matrix into an object with some aditional properties
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
@@ -12,6 +17,8 @@ makeCacheMatrix <- function(x = matrix()) {
        getinverse = getinv)
 }
 
+#cacheSolve indicates whether we have a value of inverse matrix or no. 
+#If we have it, the function ignores calculations and takes the information about reverse matrix from cache
 cacheSolve <- function(x, ...) {
   inv <- x$getinverse()
   if(!is.null(inv)) {
